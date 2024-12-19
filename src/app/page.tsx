2fb7a1954eac95f46cbac3b2fc561405e3413bc2
Abhipsit <renubajpai9@gmail.com>
2024-12-20 00:34:58 +0530

    Added navbar and footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Add features here, if you want to change the tabs in navigation bar, go to components/nav-bar and in the array of navItems change the specific tabs<br/>
-      And alool jalool code daalne se pehle please study the prewritten code, also the front-end home folder shall be deleted in future but right now it is worthy for it comntains some content
+      And alool jalool code daalne se pehle please study the prewritten code<br/>Also the front-end home folder shall be deleted in future but right now it is worthy for it comntains some content
     </div>
   );
 }
